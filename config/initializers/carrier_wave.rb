4688d7a96cbfc5a44d6027427797361863bd2f64
Wen Jen <wjen85@gmail.com>
2016-06-10 12:19:36 -0700

    fix syntax

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
   config.fog_credentials = {
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    :region                 => ENV[’S3_REGION’]
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region                => ENV['S3_REGION']
   }
     config.fog_directory    = ENV['S3_BUCKET_NAME ']
   end
