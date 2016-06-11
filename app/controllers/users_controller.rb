e876c25900468936b7b0b79ea69c0d78cdc1cce9
Wen Jen <wjen85@gmail.com>
2016-06-11 15:03:43 -0700

    fix update settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,13 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :password_confirmation, :handle, :statement)
   end
 
-  def
+  #confirms a logged in user
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
   #confirms correct user
   def correct_user
